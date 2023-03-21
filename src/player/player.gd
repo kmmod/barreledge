2d06dc0ece52a29860f475f15b0b34a0e7b17830
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-03-21 01:04:39 +0100

    init

---
+++ src/player/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY: float = 0.01
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera: Camera3D = $Camera3D
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		handle_mouse_input(event.relative)
+
+
+func handle_mouse_input(mouse_delta):
+		var mouse_x = -mouse_delta.x * SENSITIVITY
+		var mouse_y = -mouse_delta.y * SENSITIVITY
+
+		camera.rotate_x(mouse_y)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		self.rotate_y(mouse_x)
