c97c4fa81d6bf90cdb02c52e94103c9a2c04aa88
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-03-26 13:28:53 +0200

    Drone enemy

---
+++ src/enemy/enemy_drone.gd
@@ -0,0 +1,61 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var detection_range = 20
+@export var attack_range = 10
+
+# Those should be controlled by turret component and by
+# amunition type instead of hard coded
+@export var attack_damage = 10
+@export var attack_speed = 1.0
+
+var player = null
+
+
+var attack_timer = 0.0
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+enum State {
+  IDLE,
+  ATTACK,
+  DEAD,
+  CHASE,
+  PATROL,
+  RUN,
+}
+
+var current_state = State.IDLE
+
+# TODO: Whats ia a better way to handle dependencies?
+# Maybe with export node path?
+@onready var eye: MeshInstance3D = $Eye
+@onready var turret: MeshInstance3D = $Turret
+
+
+func _ready():
+  # TODO: Player node should be defined as global variable
+  # instead of relying on absolute path
+  player = get_node("/root/main/Player")
+  eye.rotation_change.connect(on_eye_rotation_change)
+
+
+func _process(delta) -> void:
+  match current_state:
+    State.IDLE:
+      pass
+    State.ATTACK:
+      pass
+    State.DEAD:
+      pass
+    State.CHASE:
+      pass
+    State.PATROL:
+      pass
+    State.RUN:
+      pass
+
+
+func on_eye_rotation_change(eye_rotation: Vector3) -> void:
+  print("Changed", eye_rotation)
+  turret.rotation = eye_rotation
---
+++ src/enemy/enemy_drone_eye.gd
@@ -0,0 +1,69 @@
+extends Node3D
+
+@export var angle_limit: float = 10
+
+signal rotation_change
+
+enum State {
+  IDLE,
+  FOLLOW
+}
+
+var follow_target: Vector3 = Vector3(0, 0, 0)
+
+var current_state = State.IDLE
+
+var next_idle_time = 0
+var current_time = 0
+var rotation_target = Vector2(0, 0)
+var rotation_speed = 10.0
+
+
+
+func _ready() -> void:
+  rotation_target = Vector3(0, rotation.y, rotation.z)
+
+
+func _process(delta: float) -> void:
+  # State switch is controlled by parent node
+  match current_state:
+    State.IDLE:
+      update_idle_state(delta)
+    State.FOLLOW:
+      pass
+
+
+func set_follow_state(target: Vector3) -> void:
+  follow_target = target
+  transition_to_state(State.FOLLOW)
+
+
+func set_idle_state() -> void:
+  transition_to_state(State.IDLE)
+
+
+func transition_to_state(new_state) -> void:
+  current_state = new_state
+
+
+func update_idle_state(delta: float) -> void:
+  if current_time > next_idle_time:
+    var yaw_target = deg_to_rad(randf() * angle_limit - angle_limit / 2)
+    var pitch_target = deg_to_rad(randf() * angle_limit - angle_limit / 2)
+    rotation_target = Vector3(0, yaw_target, pitch_target)
+    next_idle_time = randf() * 2
+    rotation_change.emit(rotation_target)
+    current_time = 0
+  else:
+    current_time += delta
+  
+  var step = clamp(rotation_speed * delta, 0, 1)
+  var yaw = lerp(rotation.y, rotation_target.y, step)
+  var pitch = lerp(rotation.z, rotation_target.z, step)
+
+  rotation = Vector3(0, yaw, pitch)
+
+
+func update_follow_state() -> void:
+  pass
+
