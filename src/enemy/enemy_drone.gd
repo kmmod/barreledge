93b04cb39d2110ca9fdc30dce2e554065e553011
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-03-29 21:03:19 +0200

    Proper drone and turret rotation

--- src/enemy/enemy_drone.gd
+++ src/enemy/enemy_drone.gd
@@ -27,13 +27,12 @@ enum State {
 var current_state = State.IDLE
 
 var rotation_target = Vector3(0, 0, 0)
-var rotation_speed = 2
+var rotation_speed = 4
 
 # TODO: Whats ia a better way to handle dependencies?
 # Maybe with export node path?
 @onready var eye: MeshInstance3D = $Eye
 @onready var turret: MeshInstance3D = $Turret
-@onready var helper: Node3D = $Helper
 
 
 func _ready():
@@ -108,25 +107,14 @@ func update_rotation(delta: float) -> void:
 
 
 func update_attack_state() -> void:
-	var direction = position.direction_to(player.position)
-	var angle_xz = atan2(direction.x, direction.z)
-	var angle_zy = atan2(direction.z, direction.y)
-	rotation_target = Vector3(0, angle_xz + PI / 2, 0)
+	var player_pos = player.position + Vector3(0, 1.7, 0)
+	var drone_direction = position.direction_to(player_pos)
+	var yaw = atan2(drone_direction.x, drone_direction.z)
+	rotation_target = Vector3(0, yaw + PI / 2, 0)
+	
+	var xz_distance = position.distance_to(Vector3(player_pos.x, position.y, player_pos.z))
+	var y_distance = position.y - player_pos.y
+	var pitch = atan2(y_distance, xz_distance)
 
-
-	var turret_target = Vector3(0, 0, angle_zy - PI / 2)
-	print("Direction to player", direction)
-	print("angle_xz", angle_xz)
-	print("angle_xy", angle_zy)
-
-	helper.rotation = turret_target
-
-	# direction from drone to player
-	# var forward = global_transform.basis.z
-	# var forw = Vector3.FORWARD
-	# var dir = position.direction_to(player.position)
-	# var rot = Vector3(dir.x, dir.y, dir.z)
-	# helper.rotation = rot
-	# var rot = Vector3(0, dir.y, 0)
-	# var rot = global_transform.looking_at(player.position, Vector3.UP).origin.normalized()
+	var turret_target = Vector3(0, 0, pitch)
 	turret.set_rotation_target(turret_target)
